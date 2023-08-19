fce2061944ff6dbc606606731b261367ce529187
Seth Villa <sethvilla13@gmail.com>
2023-08-19 13:21:29 -0700

    big o

---
+++ docs/Big-O/fun-challenge2.js
@@ -0,0 +1,19 @@
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+    let a = 5; // 0(1)
+    let b = 10; // 0(1)
+    let c = 50; // 0(1)
+    for (let i = 0; i < input; i++) {
+        let x = i + 1; // 0(n)
+        let y = i + 2; // 0(n)
+        let z = i + 3; // 0(n)
+
+    }
+    for (let j = 0; j < input; j++) {
+        let p = j * 2; // 0(n)
+        let q = j * 2; // 0(n)
+    }
+    let whoAmI = "I don't know"; //0(1)
+
+    // 4 +  7n = 0(n)
+}
